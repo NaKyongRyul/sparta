ae7120d0efd7f1c454b97de4c05eb57630ac79ba
NaKyongRyul <67616146+NaKyongRyul@users.noreply.github.com>
2020-09-09 12:51:52 +0900

    add

--- my_project/B대학 모여라/app.py
+++ my_project/B대학 모여라/app.py
@@ -30,7 +30,7 @@ def detail(meeting_id):
 @app.route('/meeting', methods=['POST'])
 def meeeting():
     title_receive = request.form['title_give']
-    number_receive = request.form['number_give']
+    pw_receive = request.form['pw_give']
     place_receive = request.form['place_give']
     time_receive = request.form['time_give']
     desc_receive = request.form['desc_give']
@@ -43,14 +43,14 @@ def meeeting():
     doc = {
         'id': total + 1,
         'title': title_receive,
-        'number': number_receive,
+        'pw': pw_receive,
         'place': place_receive,
         'time': time_receive,
         'desc': desc_receive,
         'todo': todo_receive,
         'keyword': keyword_receive
     }
-    print(title_receive, number_receive, place_receive, time_receive, desc_receive, todo_receive, keyword_receive)
+    print(title_receive, pw_receive, place_receive, time_receive, desc_receive, todo_receive, keyword_receive)
 
     db.meeting.insert_one(doc)
 
@@ -68,19 +68,59 @@ def meeting_delete():
     id = request.form['id']
     pw = request.form['pw']
     db.meeting.delete_one({'id': int(id), 'pw': pw})
-    print(id, pw)
     return jsonify({'result': 'success', 'msg': '모임이 삭제되었습니다.'})
 
 
 @app.route('/api/modify', methods=['POST'])
 def meeting_modify():
+
     # 수정된 글 정보를 입력받음
     id = request.form['id']
-    pw = request.form['pw']
     # 비밀번호도 입력받아야함
+    title_receive = request.form['title_give']
+    pw_receive = request.form['pw_give']
+    place_receive = request.form['place_give']
+    time_receive = request.form['time_give']
+    desc_receive = request.form['desc_give']
+    todo_receive = request.form['todo_give']
+    keyword_receive = request.form['keyword_give']
 
-    db.meeting.update_one({'id': id, 'number': pw}, {'$set': {}})
-    return
+    # meeing 고유 식별 키 만들기
+    total = db.meeting.count()
+
+    doc = {
+        'id': total + 1,
+        'title': title_receive,
+        'pw': pw_receive,
+        'place': place_receive,
+        'time': time_receive,
+        'desc': desc_receive,
+        'todo': todo_receive,
+        'keyword': keyword_receive
+    }
+    print(title_receive, pw_receive, place_receive, time_receive, desc_receive, todo_receive, keyword_receive)
+
+    db.meeting.insert_one(doc)
+
+    meetings = db.meeting.find_one({'id': id})
+
+    new_title = meetings['title']
+    new_pw = meetings['pw']
+    new_place = meetings['place']
+    new_time = meetings['time']
+    new_desc = meetings['desc']
+    new_todo = meetings['todo']
+    new_keyword = meetings['keyword']
+
+    db.meeting.update_one({'id': id}, {'$set': {'title': new_title,
+                                                'pw': new_pw,
+                                                'place': new_place,
+                                                'time': new_time,
+                                                'desc': new_desc,
+                                                'todo': new_todo,
+                                                'keyword': new_keyword}
+                                                 })
+    return jsonify({'result': 'success', 'msg': '수정이 완료되었습니다.'})
 
 
 @app.route('/api/auth', methods=['POST'])
@@ -88,7 +128,7 @@ def meeting_auth():
     id = request.form['id']
     pw = request.form['pw']
     # id와 pw가 일치하는 meeting이 있는지 확인
-    meeting = db.meeting.find_one({'id': int(id), 'number': pw})
+    meeting = db.meeting.find_one({'id': int(id), 'pw': pw})
     if meeting is None:
         # 일치하지 않는 경우 return fail
         return jsonify({'result': 'fail'})
